14ec8c97ea5ac4c2d8091376745ee0e2db0f527e
Diogo Trigueiros <121031730+diogotrigueiros@users.noreply.github.com>
2023-11-24 14:24:17 +0000

    russianEmpire24Nov
    Second version of the project for pw2

---
+++ Russian Empire/processar_login.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "user"; 
+$password = "password"; 
+$dbname = "russianEmpire"; 
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Erro de conexão: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST["username"];
+    $password = $_POST["password"];
+
+    $sql = "SELECT * FROM tlogin WHERE user = '$username' AND pass = sha1('$password')";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        header("Location: index.html");
+    } else {
+        echo "Credenciais inválidas. Tente novamente.";
+    }
+}
+
+$conn->close();
+?>
